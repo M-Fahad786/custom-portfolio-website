46b6f974280e3819227e12a553b67bd76f6188cc
M-Fahad786 <fahad.zahid2004@gmail.com>
2023-03-05 01:14:39 +0500

    initial stages

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import "./header.css";
-export const Header = () => {
+import React from 'react'
+import "./CTA"
+import "./header.css"
+const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Muhammad Fahad</h1>
-        <h5 className="text-light"></h5>
+        <h1>Hello </h1>
       </div>
     </header>
-  );
-};
+  )
+}
+
+export default Header
